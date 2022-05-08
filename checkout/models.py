f86dbda82185ceba3cf33ecd420f74ac2f9a3dad
J-R77-stack <johnnyramsay81@gmail.com>
2022-05-08 09:41:43 +0000

    add forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -1,24 +1,23 @@
 import uuid
-from django.db import models
 
-from django_countries.fields import CountryField
+from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from django_countries.widgets import CountrySelectWidget
 
-class checkout_order (models.Model):
-    shipping_address = models.CharField(max_length=80, null=False, blank=False)
-    shipping_address2 = models.CharField(max_length=80, null=False, blank=False)
-    shipping_country = CountryField(blank_label='(select country)').formfield(
-        required=False,
-        widget=CountrySelectWidget(attrs={
-            'class': 'custom-select d-block w-100',
-        }))
-    shipping_postcode = models.CharField(max_length=20, null=True, blank=True)
+from products.models import Product
+
+
+class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
@@ -30,8 +29,6 @@ class checkout_order (models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-
-    
     def update_total(self):
         """
         Update grand total each time a line item is added,
@@ -57,5 +54,20 @@ class checkout_order (models.Model):
     def __str__(self):
         return self.order_number
 
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
