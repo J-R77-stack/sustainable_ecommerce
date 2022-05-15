f3d037db464aa5acc1779fcfd5b9e486ee52be74
J-R77-stack <johnnyramsay81@gmail.com>
2022-05-15 09:06:59 +0000

    fixed migration issues

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django_countries.fields import CountryField
+
 
 class UserProfile(models.Model):
     """
@@ -17,7 +19,7 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_country = models.CharField(max_length=40, null=False, blank=False)
+    default_country = CountryField(blank_label='GB', null=False, blank=False)
 
     def __str__(self):
         return self.user.username
