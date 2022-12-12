849d7198142ddc97c185644e32c95366a9ff1723
J-R77-stack <johnnyramsay81@gmail.com>
2022-12-12 16:17:54 +0000

    new deploy

--- sustainable/settings.py
+++ sustainable/settings.py
@@ -31,13 +31,10 @@ DEBUG = False
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sustainable2022.herokuapp.com', 'localhost']
- # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
-
-
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+  ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
